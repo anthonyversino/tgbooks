8c345b4908b5816120b5c89c497250b0138d8f64
Anthony Versino <anthony.versino@gmail.com>
2015-02-25 16:26:07 -0600

    Firebase integration error fixing. Post.get
    Forgot to add post.get to fill the scope

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,7 +1,7 @@
 'use strict';
 
 app.controller('PostsCtrl', function($scope, Post) {
-  $scope.posts = [];
+  $scope.posts = Post.get();
   $scope.post = {url: 'http://', title: ''};
 
   $scope.submitPost =  function () {
